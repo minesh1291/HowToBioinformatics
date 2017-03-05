3fe465e51c560a352e09861e795d859cafa83e53
Minesh Jethva <minesh.1291@gmail.com>
2017-03-05 07:15:30 +0200

    Create topGOv3.r

---
+++ EnrichmentAnalysis/topGOv3.r
@@ -0,0 +1,104 @@
+library("topGO")
+library("org.Hs.eg.db")
+
+makeGOResult= function(GOdata,WriteFile,myInterestedGenes){
+  
+  # test.stat <- new("elimCount", testStatistic = GOFisherTest, name = "Fisher test")
+  # result.E.Fish <- getSigGroups(GOdata, test.stat)
+  
+  # test.stat <- new("elimScore", testStatistic = GOKSTest, name = "KS tests")
+  # result.E.KS <- getSigGroups(GOdata, test.stat)
+  
+  test.stat <- new("classicCount", testStatistic = GOFisherTest, name = "Fisher test")
+  result.C.Fish <- getSigGroups(GOdata, test.stat)
+  
+  test.stat <- new("classicScore", testStatistic = GOKSTest, name = "KS tests")
+  result.C.KS <- getSigGroups(GOdata, test.stat)
+  # whichAlgorithms()
+  # whichTests()
+  
+  allGO = usedGO(object = GOdata) 
+  # use it in GenTable as follows:
+  
+  allRes <- GenTable(GOdata, 
+                     classicFisher = result.C.Fish, 
+                     # elimFisher = result.E.Fish, 
+                     classicKS = result.C.KS, 
+                     # elimKS = result.E.KS, 
+                     numChar=100,orderBy = "classicFisher", ranksOf = "classicFisher", topNodes = length(allGO)
+  )
+  
+  class(allRes)#data.frame
+  allRes$FoldEnrichment = allRes$Significant/allRes$Expected
+  # sum(is.na(allRes$classicFisher))
+  allRes$FDR.classicFisher = p.adjust(allRes$classicFisher,method = "BH")
+  allRes$FDR.classicKS = p.adjust(allRes$classicKS,method = "BH")
+  bg.geneList=genesInTerm(GOdata, whichGO=allRes$GO.ID)
+  bg.geneList.v2 = lapply(bg.geneList,function(x) paste(x,collapse=", ") )
+  
+  int.geneList.v1 = lapply(bg.geneList,function(x) x[x %in% myInterestedGenes] )
+  int.geneList.v2 = lapply(int.geneList.v1,FUN = function(x) paste(x,collapse=", ") )
+  int.geneList.v2 =do.call(rbind.data.frame, int.geneList.v2)
+  
+  allRes$GeneSymbols=int.geneList.v2[allRes$GO.ID,1]
+  
+  write.csv(allRes,WriteFile)
+}
+
+
+
+
+makeHs.GOResult= function(myInterestedGenes,OntoType,WriteFile){
+  #myInterestedGenes = HUGO_Symbols
+  #OntoType = BP, CC, MF 
+  #WriteFile = csv filename with path
+  
+  ## GO to Symbol mappings (only the BP ontology is used)
+  xx <- annFUN.org(OntoType, mapping = "org.Hs.eg.db", ID = "symbol")
+  # head(xx)
+  allGenes <- unique(unlist(xx))
+  # myInterestedGenes <- sample(allGenes, 500)
+  geneList <- factor(as.integer(allGenes %in% myInterestedGenes ))
+  names(geneList) <- allGenes
+  
+  GOdata <- new("topGOdata",
+                ontology = OntoType,
+                allGenes = geneList,
+                nodeSize = 5,
+                annot = annFUN.org, 
+                mapping = "org.Hs.eg.db",
+                ID = "symbol") 
+  
+  makeGOResult(GOdata,WriteFile,myInterestedGenes)
+  
+}
+
+####################
+
+
+makeHs.udefined.BG.GOResult= function(allGenes,myInterestedGenes,OntoType,WriteFile){
+  #myInterestedGenes = HUGO_Symbols
+  #OntoType = BP, CC, MF 
+  #WriteFile = csv filename with path
+  
+  ## GO to Symbol mappings (only the BP ontology is used)
+  xx <- annFUN.org(OntoType, mapping = "org.Hs.eg.db", ID = "symbol")
+  # head(xx)
+  allGenes_Hs <- unique(unlist(xx))
+  # allGenes <- IAG_names[IAG_names %in% allGenes_Hs]
+  allGenes <- allGenes[allGenes %in% allGenes_Hs]
+  # myInterestedGenes <- sample(allGenes, 500)
+  geneList <- factor(as.integer(allGenes %in% myInterestedGenes ))
+  names(geneList) <- allGenes
+  
+  GOdata <- new("topGOdata",
+                ontology = OntoType,
+                allGenes = geneList,
+                nodeSize = 5,
+                annot = annFUN.org, 
+                mapping = "org.Hs.eg.db",
+                ID = "symbol") 
+  
+  makeGOResult(GOdata,WriteFile,myInterestedGenes)
+  
+}
