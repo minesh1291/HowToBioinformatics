8933b5e2efb08ae91d1d67a27cad50731c3a3bdd
Minesh A. Jethva <minesh.1291@gmail.com>
2017-11-18 00:28:07 +0200

    Update topGOv3.r

--- EnrichmentAnalysis/topGOv3.r
+++ EnrichmentAnalysis/topGOv3.r
@@ -1,15 +1,15 @@
 library("topGO")
 mappingDB="org.Hs.eg.db"
-library(mappingDB)
+library("org.Hs.eg.db")
 
 
 makeGOResult= function(GOdata,WriteFile,myInterestedGenes){
   
-  # test.stat <- new("elimCount", testStatistic = GOFisherTest, name = "Fisher test")
-  # result.E.Fish <- getSigGroups(GOdata, test.stat)
+  test.stat <- new("elimCount", testStatistic = GOFisherTest, name = "Fisher test")
+  result.E.Fish <- getSigGroups(GOdata, test.stat)
   
-  # test.stat <- new("elimScore", testStatistic = GOKSTest, name = "KS tests")
-  # result.E.KS <- getSigGroups(GOdata, test.stat)
+  test.stat <- new("elimScore", testStatistic = GOKSTest, name = "KS tests")
+  result.E.KS <- getSigGroups(GOdata, test.stat)
   
   test.stat <- new("classicCount", testStatistic = GOFisherTest, name = "Fisher test")
   result.C.Fish <- getSigGroups(GOdata, test.stat)
@@ -24,17 +24,20 @@ makeGOResult= function(GOdata,WriteFile,myInterestedGenes){
   
   allRes <- GenTable(GOdata, 
                      classicFisher = result.C.Fish, 
-                     # elimFisher = result.E.Fish, 
-                     classicKS = result.C.KS, 
-                     # elimKS = result.E.KS, 
+                     elimFisher = result.E.Fish,
+                     classicKS = result.C.KS,
+                     elimKS = result.E.KS,
                      numChar=100,orderBy = "classicFisher", ranksOf = "classicFisher", topNodes = length(allGO)
   )
   
   class(allRes)#data.frame
   allRes$FoldEnrichment = allRes$Significant/allRes$Expected
   # sum(is.na(allRes$classicFisher))
-  allRes$FDR.classicFisher = p.adjust(allRes$classicFisher,method = "BH")
-  allRes$FDR.classicKS = p.adjust(allRes$classicKS,method = "BH")
+  allRes$FDR.classicFisher = p.adjust(allRes$classicFisher,method = "fdr")
+  allRes$FDR.classicKS = p.adjust(allRes$classicKS,method = "fdr")
+  allRes$FDR.elimFisher = p.adjust(allRes$elimFisher,method = "fdr")
+  allRes$FDR.elimKS = p.adjust(allRes$elimKS,method = "fdr")
+  
   bg.geneList=genesInTerm(GOdata, whichGO=allRes$GO.ID)
   bg.geneList.v2 = lapply(bg.geneList,function(x) paste(x,collapse=", ") )
   
@@ -42,7 +45,14 @@ makeGOResult= function(GOdata,WriteFile,myInterestedGenes){
   int.geneList.v2 = lapply(int.geneList.v1,FUN = function(x) paste(x,collapse=", ") )
   int.geneList.v2 =do.call(rbind.data.frame, int.geneList.v2)
   
-  allRes$GeneSymbols=int.geneList.v2[allRes$GO.ID,1]
+  print(class(int.geneList.v2))
+  print(head(int.geneList.v2))
+  
+  allRes$GeneSymbols=int.geneList.v2[,1] #[allRes$GO.ID,1]
+  
+  print(class(allRes$GeneSymbols))
+  print(head(allRes$GeneSymbols))
+  
   
   write.csv(allRes,WriteFile)
 }
